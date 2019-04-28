2c1a232084764751dcd85c25bfbe859cb09fb49f
Someone Else <rsatarov1999@gmail.com>
2019-04-28 19:33:53 +0300

    Added note deleting by swipe left. Deleted NoteList class and layout.

--- app/src/main/java/com/cit17b/studays/note/NoteListActivity.java
+++ app/src/main/java/com/cit17b/studays/note/NoteListActivity.java
@@ -1,6 +1,7 @@
 package com.cit17b.studays.note;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -9,29 +10,40 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.cit17b.studays.DBHelper;
 import com.cit17b.studays.R;
-import com.cit17b.studays.note.CreateNoteActivity;
-import com.cit17b.studays.note.NoteList;
+
+import java.util.ArrayList;
 
 /**
- * Класс представляет собой Activity, что отображает список всех всех заметок.
+ * Класс представляет собой Activity, что отображает список всех заметок.
  *
  * @author Ruslan Satarov
- * @version 1.1
+ * @version 1.2
  */
 public class NoteListActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**
-     * Список с заметками.
+     * Список, содержащий заметки, что будут отображены.
      */
-    private NoteList noteList;
+    private ArrayList<Note> dataList;
 
     /**
-     * Кнопка вызова меню создания заметки.
+     * Список, что отображает на єкране заметки.
      */
-    private FloatingActionButton createNoteButton;
+    private SwipeMenuListView listView;
+
+    /**
+     * Объект для работы с базой данных.
+     */
+    private DBHelper dbHelper;
 
     /**
      * Вызывается при создании Activity.
@@ -43,11 +55,16 @@ public class NoteListActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note_list);
+        setContentView(R.layout.activity_swipe_note_list);
 
-        noteList = (NoteList) getSupportFragmentManager().findFragmentById(R.id.noteList);
-        createNoteButton = findViewById(R.id.createNoteButton);
+        FloatingActionButton createNoteButton = findViewById(R.id.createNoteButton);
         createNoteButton.setOnClickListener(this);
+
+        dbHelper = new DBHelper(this);
+
+        dataList = new ArrayList<>();
+        listView = getAdjustedSwipeMenuListView();
+        updateList();
     }
 
     /**
@@ -67,9 +84,8 @@ public class NoteListActivity extends AppCompatActivity implements View.OnClickL
      * Вызывается при выборе элемента меню.
      *
      * @param item Выбранный элемент меню.
-     *
      * @return Верните false, чтобы разрешить нормальную обработку меню,
-     *         true, чтобы использовать ее здесь.
+     * true, чтобы использовать ее здесь.
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -78,12 +94,11 @@ public class NoteListActivity extends AppCompatActivity implements View.OnClickL
                 finish();
                 break;
             case R.id.deleteAllNotesButton:
-                DBHelper dbHelper = new DBHelper(this);
                 SQLiteDatabase database = dbHelper.getWritableDatabase();
                 database.delete(getString(R.string.table_notes_name), null, null);
                 database.close();
                 dbHelper.close();
-                noteList.onUpdate();
+                updateList();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -109,14 +124,102 @@ public class NoteListActivity extends AppCompatActivity implements View.OnClickL
      * было вызвано, resultCode и, возможно, дополнительные данные.
      *
      * @param requestCode Код, с которым было вызвано Activity.
-     * @param resultCode Код, идентифицирующий результат работы дочернего Activity.
-     * @param data Intent, который может содержать результирующие данные.
+     * @param resultCode  Код, идентифицирующий результат работы дочернего Activity.
+     * @param data        Intent, который может содержать результирующие данные.
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
-            noteList.onUpdate();
+            updateList();
         }
+    }
+
+    /**
+     * Возвращает настроенный для заметок список.
+     *
+     * @return Настроенный объект SwipeMenuListView.
+     */
+    private SwipeMenuListView getAdjustedSwipeMenuListView() {
+        SwipeMenuListView listView = findViewById(R.id.noteSwipeListView);
+
+        ArrayAdapter<Note> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(adapter);
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getApplicationContext());
+                deleteItem.setBackground(R.color.colorPrimary);
+                deleteItem.setWidth((int) (50 * getResources().getDisplayMetrics().density));
+                deleteItem.setIcon(R.drawable.ic_delete_white_24dp);
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        listView.setMenuCreator(creator);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int noteIdSelected = dataList.get(position).getId();
+                Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+                intent.putExtra("requestCode", CreateNoteActivity.REQUEST_CODE_EDIT_NOTE);
+                intent.putExtra("id", noteIdSelected);
+                startActivityForResult(intent, CreateNoteActivity.REQUEST_CODE_EDIT_NOTE);
+            }
+        });
+
+        listView.setOnSwipeListener(new SwipeMenuListView.OnSwipeListener() {
+            @Override
+            public void onSwipeStart(int position) {
+                // doing nothing here
+            }
+
+            @Override
+            public void onSwipeEnd(int position) {
+                if (position != -1) {
+                    SQLiteDatabase database = dbHelper.getWritableDatabase();
+                    database.delete(getString(R.string.table_notes_name), "id = ?", new String[]{String.valueOf(dataList.get(position).getId())});
+                    database.close();
+                    dbHelper.close();
+                    updateList();
+                }
+            }
+        });
+
+        return listView;
+    }
+
+    /**
+     * Обновляет содержимое списка.
+     */
+    private void updateList() {
+        fillDataListFromDB();
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList));
+    }
+
+    /**
+     * Заполняет список данными из базы данных.
+     */
+    private void fillDataListFromDB() {
+        dataList.clear();
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        Cursor cursor = database.query(getString(R.string.table_notes_name), null, null, null, null, null, "id DESC");
+        if (cursor.moveToFirst()) {
+            int idColIndex = cursor.getColumnIndex("id");
+            int titleColIndex = cursor.getColumnIndex("title");
+            int noteTextColIndex = cursor.getColumnIndex("noteText");
+            do {
+                Note note = new Note();
+                note.setId(cursor.getInt(idColIndex));
+                note.setTitle(cursor.getString(titleColIndex));
+                note.setNoteText(cursor.getString(noteTextColIndex));
+                dataList.add(note);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        database.close();
+        dbHelper.close();
     }
 }
